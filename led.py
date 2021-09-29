505cde0731e9841ea06c214adf3ba4f789af1a42
MoreIceFitzGerry <maurice@fitzgerald.ch>
2021-09-29 21:04:37 +0200

    Merging Files from Old Raspberry to Repo.

--- led.py
+++ led.py
@@ -4,10 +4,12 @@
 #
 # Direct port of the Arduino NeoPixel library strandtest example.  Showcases
 # various animations on a strip of NeoPixels.
-
+import random
+import math
 import time
 from neopixel import *
 import argparse
+import threading
 
 # LED strip configuration:
 LED_COUNT      = 90      # Number of LED pixels.
@@ -19,24 +21,96 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor$
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+blueLastTime = 0
 
+def current_milli_time():
+    return round(time.time() * 1000)
 
 # Define functions which animate LEDs in various ways.
-def colorInline(strip, color, wait_ms=10):
+def colorInline(strip, color, wait_ms=1):
     """Wipe color across display a pixel at a time."""
-    for i in range (25):
+    for i in range (0, 25):
         strip.setPixelColor(i, color)
 #       if i>6:
 #               strip.setPixelColor(LED_COUNT-i+6, Color(0,0,0)) 
-        strip.show()
+        #strip.show()
         time.sleep(wait_ms/1000.0)
 
+def blueTaetschBang(strip, wait_ms=1):
+    global blueLastTime
+    """HAVE EACH LED ALTERNATE THROUGH BLUE AT RANDOM"""
+    greenred = 0 # Default value to adjust green and red simultaneously to adjust brightness of blue leds
+    currentMillis = current_milli_time()
+    if ( blueLastTime < currentMillis - 200):
+        blueLastTime = currentMillis
+        for i in range (25,88):
+            greenred = abs(20*int(math.cos(((i-random.randint(0,24))*math.pi)/8)))
+            color = Color(greenred,greenred,255)    
+            strip.setPixelColor(i, color) 
+    #strip.show()
+    #time.sleep(wait_ms/1000.0)
+
+def launchSwipe(strip, color, wait_ms=10):
+        for i in range(25,0,-1):
+            strip.setPixelColor(i, color)
+            #strip.show()
+            time.sleep(wait_ms/1000.0)
+
+
+def colorBottle(strip, color, wait_ms=10):
+    strip.setPixelColor(88, color)
+    #strip.show()
+
+def colorBottleSelect(strip, wait_ms=10):
+    strip.setPixelColor(88, Color(255,0,0))
+    print("255")
+    strip.show()
+    time.sleep(wait_ms/10.0)
+
+    strip.setPixelColor(88, Color(205,0,0))
+    print("205")
+    strip.show()
+    time.sleep(wait_ms/10.0)
+
+    strip.setPixelColor(88, Color(155,0,0))
+    print("155")
+    strip.show()
+    time.sleep(wait_ms/10.0)
+
+    strip.setPixelColor(88, Color(105,0,0))
+    print("105")
+    strip.show()
+    time.sleep(wait_ms/10.0)
+
+    strip.setPixelColor(88, Color(55,0,0))
+    print("55")
+    strip.show()
+    time.sleep(wait_ms/10.0)
+
+    strip.setPixelColor(88, Color(0,0,0))
+    print("0")
+    strip.show()
+    time.sleep(wait_ms/10.0)
+
+
+def colorWipe(strip, color, wait_ms=10):
+    """Wipe color across display a pixel at a time."""
+    for i in range(LED_COUNT+1):
+        strip.setPixelColor(LED_COUNT-i, color)
+#       if i>6:
+#               strip.setPixelColor(LED_COUNT-i+6, Color(0,0,0)) 
+        #strip.show()
+        time.sleep(wait_ms/1000.0)
+
+
+
+#### ----- ORIGINAL (Unused) COLORPIPE --------
 def colorPipe(strip, color, wait_ms=10):
     """Wipe color across display a pixel at a time."""
     for i in range (25,89):
         strip.setPixelColor(i, color)
-#       if i>6:
-#               strip.setPixelColor(LED_COUNT-i+6, Color(0,0,0)) 
+        if i>6:
+            strip.setPixelColor(LED_COUNT-i+6, Color(0,0,0)) 
         strip.show()
         time.sleep(wait_ms/1000.0)
 
@@ -44,16 +118,6 @@ def brightnessPipe(strip, brightness, wait_ms=10):
     """Wipe color across display a pixel at a time."""
     for i in range (25,89):
         strip.setBrightness(i, brightness)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-
-def colorWipe(strip, color, wait_ms=10):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(LED_COUNT-i, color)
-#       if i>6:
-#               strip.setPixelColor(LED_COUNT-i+6, Color(0,0,0)) 
         strip.show()
         time.sleep(wait_ms/1000.0)
 
@@ -115,22 +179,39 @@ if __name__ == '__main__':
     if not args.clear:
         print('Use "-c" argument to clear LEDs on exit')
 
+
+    
     try:
 
         while True:
             
-            print ('Color Test to find positions')
-            colorInline(strip, Color(0,0,0)) # Red wipe on strip based of pixel number
-            colorInline(strip, Color(255,255,255)) # Red wipe on strip based of pixel number
+            #print ('Color Test to find positions')
+            #colorInline(strip, Color(0,0,0)) # Red wipe on strip based of pixel number
+            #colorInline(strip, Color(80,80,255)) # Red wipe on strip based of pixel number
+            
+            
+            ####################colorInline(strip, Color(0,255,0))
 
             #colorPipe(strip, Color(255,0,0)) # Green wipe on strip
             #colorPipe(strip, Color(0,255,0)) # Red wipe on strip
-            #colorPipe(strip, Color(0,0,255)) # Blue wipe on strip
+            #colorPipe(strip, Color(40,40,255)) # Blue wipe on strip
+            #colorPipe(strip, Color(greenred,greenred,55))
             
-            brightnessPipe(strip, 255) # Black wipe on strip
-            brightnessPipe(strip, 160) # Black wipe on strip
-            brightnessPipe(strip, 120) # Black wipe on strip
-            brightnessPipe(strip, 160) # Black wipe on strip
+        
+            blueTaetschBang(strip)
+            
+            ####################launchSwipe(strip, Color(255,255,255))
+
+
+            ####################colorBottle(strip, Color(105,0,0))
+            #colorBottleSelect(strip)
+
+
+
+            #brightnessPipe(strip, 255) # Black wipe on strip
+            #brightnessPipe(strip, 160) # Black wipe on strip
+            #brightnessPipe(strip, 120) # Black wipe on strip
+            #brightnessPipe(strip, 160) # Black wipe on strip
 
             #print ('Color wipe animations.')
             #colorWipe(strip, Color(255, 0, 0))  # Red wipe
@@ -146,7 +227,7 @@ if __name__ == '__main__':
             #rainbow(strip)
             #rainbowCycle(strip)
             #theaterChaseRainbow(strip)
-            strip
+            strip.show()
         
     except KeyboardInterrupt:
         if args.clear:
