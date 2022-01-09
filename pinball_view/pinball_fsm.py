909a8db4f5534263f4609c03f36d844732eee688
Lukas von Däniken <lukas.vondaeniken@icloud.com>
2022-01-09 22:51:57 +0100

    create statemachine for top level

--- pinball_view/pinball_fsm.py
+++ pinball_view/pinball_fsm.py
@@ -1,11 +1,43 @@
 from statemachine import StateMachine, State
 
 class PinballStatemachine(StateMachine):
-    step1 = State('step1', init=True)
-    step2 = State('step2')
-    step3 = State('step3')
-    step4 = State('step4')
-    step5 = State('step5')
-    step6 = State('step6')
-    step7 = State('step7')
-    step8 = State('step8')
+    step1 = State('Sammle Malz')
+    step2 = State('Animation Mahlen')
+    step3 = State('Mische das Malz')
+    step4 = State('Animation Läutern')
+    step5 = State('Aktivierung Hopfen')
+    step6 = State('Sammle Hopfen')
+    step7 = State('Hopfen einfüllen')
+    step8 = State('Animation boiling')
+    step9 = State('Hefe züchten')
+    step10 = State('Hefe beifügen')
+    step11 = State('Animation fermentieren')
+    step12 = State('final mode')
+
+    def on_enter_step1(self):
+        ballshooter.trigger()
+        bumper.start_collecting_malt()
+
+    def on_enter_step2(self):
+        pass
+    
+    def on_enter_step3(self):
+        pass
+    def on_enter_step4(self):
+        pass
+    def on_enter_step5(self):
+        pass
+    def on_enter_step6(self):
+        pass
+    def on_enter_step7(self):
+        pass
+    def on_enter_step8(self):
+        pass
+    def on_enter_step9(self):
+        pass
+    def on_enter_step10(self):
+        pass
+    def on_enter_step11(self):
+        pass
+    def on_enter_step12(self):
+        pass
