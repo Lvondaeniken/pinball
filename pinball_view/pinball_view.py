f4afe167bb276e206c075e2ae71b8ad535c4ad95
Lukas von Daeniken <lukas.vondaeniken@icloud.com>
2022-02-06 12:05:36 +0100

    obsolete import

--- pinball_view/pinball_view.py
+++ pinball_view/pinball_view.py
@@ -3,7 +3,6 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.audio import Sound, SoundLoader
 from kivy.clock import Clock
-from pinball_listener import pinball_listener
 from screen_highscore import HighscoreScreen
 from screen_start import StartScreen
 from screen_settings import SettingsScreen
