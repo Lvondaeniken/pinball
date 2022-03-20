3e3c381caf4aafb4db9997360644a08f887cc333
Lukas von Daeniken <lukas.vondaeniken@icloud.com>
2022-03-20 16:38:34 +0100

    asdf

--- led_manager/blinking.py
+++ led_manager/blinking.py
@@ -6,7 +6,7 @@ class BlinkingLight:
         self.color = color
         self.frames_to_survive = duration_s*1000/timebase_ms
         self.half_period_frames = 1000/(2*frequency_hz*timebase_ms)
-        print(self.half_period_frames)
+        print(f'half period {self.half_period_frames}')
         self.led_count = led_count
         self.frame_counter = 0
         self.background_color = background
@@ -19,6 +19,7 @@ class BlinkingLight:
             print(self.frame_counter)
             # check if half period has past
             if self.frame_counter % self.half_period_frames == 0:
+                print((self.frame_counter/self.half_period_frames)%2)
                 #check if it is an even or odd half period
                 if (self.frame_counter/self.half_period_frames)%2 == 0:
                     for i in range(len(self.led_states)):
@@ -38,8 +39,5 @@ if __name__ == '__main__':
     b = BlinkingLight(100, 10, 1, 10, LedColor(50,100,100), LedColor(100,0,0))
     for i in range (20):
         ledstates = b.get_next_frame()
-        for ledcolor in ledstates:
-            print(ledcolor.red)
-        print("--------------------")
 
 
