c21d37ba43a6efaf03c5cc89a707e039d24dc355
Lukas von Daeniken <lukas.vondaeniken@csa.ch>
2022-06-27 06:41:01 +0200

    change questinterface

--- game_logic/Quests/Questbase.py
+++ game_logic/Quests/Questbase.py
@@ -1,13 +1,8 @@
 from abc import ABC, abstractmethod
 from pinball_hardware.pinball_machine import PinballMachine
-from game_logic.event import PinballEvent
 class Questbase(ABC):
     @abstractmethod
     def register_pinball_machine(self, machine: PinballMachine):
-        pass
-
-    @abstractmethod
-    def report_event(self, event: PinballEvent):
         pass
 
     @abstractmethod
