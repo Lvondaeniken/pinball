0f16e2d85131608266b961775974d8f2c8a4b5b0
Lukas von Däniken <lukas.vondaeniken@icloud.com>
2022-12-01 16:04:26 +0100

    ff

--- view/screens/screen_end_info_1.py
+++ view/screens/screen_end_info_1.py
@@ -3,13 +3,46 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+text1 = (
+    "Das Bier ist bereit abgezapft zu werden.\n"
+    "Treffe möglichst viele beliebige Elemente\n"
+    "bevor die Zeit abläuft um das Ziel von 200\n"
+    "Flaschen zu erreichen!"
+)
+
+text2 = (
+    "Diese Spielelemente geben dir folgende Flaschen:\n"
+    "- Kessel - 1 Flasche\n"
+    "- Ramp - 3 Flaschen\n"
+    "- Bumper - 1 Flasche\n"
+    "- Target - 2 Flaschen\n"
+    "- Kickhole - 5 Flaschen\n"
+    "Zusätzliche 20 Flaschen\n"
+    "wenn alle Elemente 1x getroffen wurden.\n"
+    "+ 5 Sek. Zeit auf Countdown." 
+)
+text3 = "OKAY LET'S GO"
+txt = [text1, text2, text3]
+
+
 class EndInfo1Screen(Screen):
     def __init__(self, *args, **kwargs):
         super(EndInfo1Screen, self).__init__(*args, **kwargs)
+        self.txt_index = 0
         for l in self.ids.items():
             id, label = l 
             if id == 'lb0':
-                label.text ="hello"
+                label.text = txt[self.txt_index] 
+            if id == 'btn0':
+                label.bind(on_press=self.cb)
+                label.text = "Für weiter, Balltaste am unteren Tischrand betätigen"
+
+    def cb(self, i):
+        self.txt_index+=1
+        if self.txt_index==len(txt):
+            self.txt_index = 0
+        self.ids['lb0'].text = txt[self.txt_index]
+
 
 
 
