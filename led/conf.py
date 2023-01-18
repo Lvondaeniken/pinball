423a58d199ba6e76d99729f0b9c74edde36204f1
Lukas von Däniken <lukas.vondaeniken@icloud.com>
2023-01-18 21:07:15 +0100

    increse ballshooter led size

--- led/conf.py
+++ led/conf.py
@@ -4,7 +4,7 @@ from led.led_group import LedGroup
 TIMEBASE_MS = 20
 
 LED_GROUPS = {
-    LedElements.BALLSHOOTER: LedGroup(10, TIMEBASE_MS),
+    LedElements.BALLSHOOTER: LedGroup(30, TIMEBASE_MS),
     LedElements.PIPES: LedGroup(12, TIMEBASE_MS),
     LedElements.BOTTLE: LedGroup(1, TIMEBASE_MS),
     LedElements.BUMPER1: LedGroup(3, TIMEBASE_MS),
