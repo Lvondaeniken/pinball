8ae72e25355aa0d6377844d9b50318e1e21bc604
Lukas von Däniken <lukas.vondaeniken@icloud.com>
2023-02-14 16:47:47 +0100

    wip

--- pinball.py
+++ pinball.py
@@ -11,18 +11,19 @@ class PinballApp:
     def __init__(self):
         # setup view with queue and start in separate process
         view_queue = Queue()
-        hw_listener = HardwareListener()
-        hw_listener.connect()
+        self.hw_listener = HardwareListener()
+        self.hw_listener.connect()
         led_manager = LedManager()
         led_manager.startup(debug=True)
         # pinball_hw = PinballMachine(led_manager)
 
         self.gui_proc = Process(target=setup_proc, args=(view_queue,))
-        self.game = Game(hw_listener, view_queue)
+        self.game = Game(self.hw_listener, view_queue)
 
     def run(self):
         self.gui_proc.start()
         self.game.start()
+
 
 
 def setup_proc(queue):
