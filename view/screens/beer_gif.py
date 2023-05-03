92bd26ad49b3da6b643a6c5f6264cba38f68acfb
Raphi <poigel.testdriver@hotmail.com>
2023-05-03 21:05:36 +0200

    New Beer Sprite sheet

--- view/screens/beer_gif.py
+++ view/screens/beer_gif.py
@@ -1,19 +1,19 @@
 from kivy.uix.image import Image
 
-atlas_file = "atlas://view/media/beer/frame"
+atlas_file = "atlas://view/media/Beer-Complete-Spritesheet@2x/frame"
 
 
 class Beer(Image):
     time = 0.0
     rate = 0.1
-    frame = 25
+    frame = 1
 
     def update(self, dt):
         self.time += dt
         if self.time > self.rate:
             self.time -= self.rate
             self.source = atlas_file + str(self.frame)
-            self.frame = self.frame - 1
-            if self.frame == 0:
-                self.frame = 25
+            self.frame = self.frame + 1
+            if self.frame == 22:
+                self.frame = 1
 
