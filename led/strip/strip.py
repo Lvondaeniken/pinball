6565f23ba8964ac70ad43bf4b2c3d5a6f9223a20
Lukas von Daeniken <lukas.vondaeniken@icloud.com>
2023-12-01 10:35:45 +0100

    move led handler into seperate module

--- led/strip/strip.py
+++
@@ -1,9 +0,0 @@
-from led.strip.dummy_strip import DummyStrip
-
-
-def get_strip(debug: bool = True):
-    if debug is True:
-        return DummyStrip()
-    else:
-        from led.strip.ws2812 import WS2812
-        return WS2812()
